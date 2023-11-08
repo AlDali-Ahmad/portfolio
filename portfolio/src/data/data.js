a60339c93d9b739fcd710c1cd2a333a7e7925a64
daliii <daliii@gmail.com>
2023-11-08 10:32:43 +0200

    first commit

---
+++ portfolio/src/data/data.js
@@ -0,0 +1,29 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: '1.png',
+    title: "Electronic shop",
+    desc: "Online store using React js and Laravel",
+    link: "",
+    git: "https://github.com/AlDali-Ahmad/Electronic-shop.git",
+  },
+  {
+    id: 2,
+    img: '2.png',
+    title: "Educational platform",
+    desc:"Educational platform using Laravel and SQLite",
+    link: "",
+    git: "https://github.com/AlDali-Ahmad/Course_Managment.git",
+  },
+  {
+    id: 3,
+    img: '3.jpg',
+    title: "Money transfers",
+    desc: "Mobile application for money transfers using Flutter",
+    link: "",
+    git: "https://github.com/AlDali-Ahmad/Money-transfer-application.git",
+  }
+  
+];
